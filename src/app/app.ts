3fd8ce71f34e1851a4f1da6462235d113a509209
SebaMC. <sevam40@gmail.com>
2026-06-07 22:43:34 -0400

    implementar diseño con Tailwind CSS v4 y corregir configuración de compilación

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // Este selector es el que usa index.html
+  standalone: true,     // Asegúrate de que sea standalone
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('RickAndMortyApp');
 }
